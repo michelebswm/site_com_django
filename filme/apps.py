82fa6735e1f11876d94fd1da4aab0ed4d48431c1
Michele de Barros Santos <michele.fenix.17@gmail.com>
2023-10-10 20:48:23 -0300

    update superuser

--- filme/apps.py
+++ filme/apps.py
@@ -9,8 +9,8 @@ class FilmeConfig(AppConfig):
         from .models import Usuario
         import os
 
-        email = os.getenv('EMAIL_ADMIN')
-        senha = os.getenv('SENHA_ADMIN')
+        email = os.getenv("EMAIL_ADMIN")
+        senha = os.getenv("SENHA_ADMIN")
 
         usuarios = Usuario.objects.filter(email=email)
         if not usuarios:
